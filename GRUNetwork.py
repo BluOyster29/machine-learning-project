808fb1d819f6ccf369ada4825a8abb7ea729984e
Rob <gusroberth@student.gu.se>
2020-01-28 13:59:50 +0100

    updating network

--- GRUNetwork.py
+++ GRUNetwork.py
@@ -5,24 +5,28 @@ import torch
 
 class ConcatPoolingGRUAdaptive(nn.Module):
     def __init__(self, vocab_size, embedding_dim, n_hidden, n_out,
-                    pretrained_vec, pretrained):
+                    pretrained_vec, pretrained, device):
         super().__init__()
         self.vocab_size = vocab_size
         self.embedding_dim = embedding_dim
         self.n_hidden = n_hidden
         self.n_out = n_out
-        if pretrained == False:
+        self.device = device
+        self.pretrained = pretrained
+        if self.pretrained == True:
             self.emb = nn.Embedding(self.vocab_size, self.embedding_dim)
-        elif pretrained == True:
             self.emb.weight.data.copy_(pretrained_vec) # load pretrained vectors
             self.emb.weight.requires_grad = False # make embedding non trainable
+        else:
+            self.emb = nn.Embedding(self.vocab_size, self.embedding_dim)
+
         self.gru = nn.GRU(self.embedding_dim, self.n_hidden)
         self.out = nn.Linear(self.n_hidden*2, self.n_out)
 
     def forward(self, seq, lengths):
         bs = seq.size(1)
         self.h = self.init_hidden(bs)
-        seq = seq.transpose(0,1).to(device)
+        seq = seq.transpose(0,1).to(self.device)
 
         embs = self.emb(seq)
         embs = embs.transpose(0,1)
@@ -38,4 +42,4 @@ class ConcatPoolingGRUAdaptive(nn.Module):
         return F.log_softmax(outp, dim=-1)
 
     def init_hidden(self, batch_size):
-       return torch.zeros((1,batch_size,self.n_hidden)).cuda().to(device)
+       return torch.zeros((1,batch_size,self.n_hidden)).cuda().to(self.device)
