85a8bd2e67732c5b91012e1a43e9e77f949b7c93
Robert <gusroberth@student.gu.se>
2020-01-28 13:32:05 +0100

    updating model

--- GRUNetwork.py
+++ GRUNetwork.py
@@ -1,28 +1,41 @@
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+import torch.nn.functional as F
 from torch import nn
 import torch
 
-class RNN_GRU(nn.Module):
-    def __init__(self, vocab_size, seq_len, input_size, hidden_size, num_layers, output_size, device, dropout=0.0):
+class ConcatPoolingGRUAdaptive(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, n_hidden, n_out,
+                    pretrained_vec, pretrained):
         super().__init__()
-        self.num_layers = num_layers
-        self.seq_len = seq_len
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.device = device
-        self.emb = nn.Embedding(vocab_size, input_size).to(device)
-        self.gru = nn.GRU(input_size, hidden_size,
-                          num_layers=self.num_layers, batch_first=True, dropout=dropout).to(device)
-        self.fc = nn.Linear(hidden_size * seq_len, output_size).to(device)
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.n_hidden = n_hidden
+        self.n_out = n_out
+        if pretrained == False:
+            self.emb = nn.Embedding(self.vocab_size, self.embedding_dim)
+        elif pretrained == True:
+            self.emb.weight.data.copy_(pretrained_vec) # load pretrained vectors
+            self.emb.weight.requires_grad = False # make embedding non trainable
+        self.gru = nn.GRU(self.embedding_dim, self.n_hidden)
+        self.out = nn.Linear(self.n_hidden*2, self.n_out)
 
-    def forward(self, sequence, hidden_layer, device):
-        output = self.emb(sequence).to(device)
-        hidden_layer = hidden_layer.to(self.device)
-        output, hidden_layer = self.gru(output, hidden_layer)
-        output = output.contiguous().view(-1, self.hidden_size *
-                                          len(sequence[0]))
-        output = self.fc(output).to(device)
+    def forward(self, seq, lengths):
+        bs = seq.size(1)
+        self.h = self.init_hidden(bs)
+        seq = seq.transpose(0,1).to(device)
 
-        return output, hidden_layer
+        embs = self.emb(seq)
+        embs = embs.transpose(0,1)
+        embs = pack_padded_sequence(embs, lengths, enforce_sorted=False)
+
+        gru_out, self.h = self.gru(embs, self.h)
+
+        gru_out, lengths = pad_packed_sequence(gru_out)
+
+        avg_pool = F.adaptive_avg_pool1d(gru_out.permute(1,2,0),1).view(bs,-1)
+        max_pool = F.adaptive_max_pool1d(gru_out.permute(1,2,0),1).view(bs,-1)
+        outp = self.out(torch.cat([avg_pool,max_pool],dim=1))
+        return F.log_softmax(outp, dim=-1)
 
     def init_hidden(self, batch_size):
-        return torch.zeros(self.num_layers, batch_size, self.hidden_size).float().to(self.device)
+       return torch.zeros((1,batch_size,self.n_hidden)).cuda().to(device)
