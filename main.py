d1cf53a984bbf207bf3defd44593f1d2645f8cbc
Robert <gusroberth@student.gu.se>
2020-01-28 14:07:21 +0100

    update main

--- main.py
+++ main.py
@@ -18,11 +18,16 @@ def get_args():
 
     parser.add_argument("-P", "--pretrained", dest='pretrained', type=str,
                         help="Use pretrained vectors (y/n)", default="n")
+    parser.add_argument("-B", "--batch_size", dest='batch_size', type=int,
+                        help="size of batches", default="100")
+    parser.add_argument("-E", "--epochs", dest='nr_epochs', type=int,
+                        help="Number of epochs", default="10")
     parser.add_argument("-D", "--glove_dimension", dest='dim', type=int,
                         help="Choose dimension for pretrained embeddings", default=50)
     parser.add_argument("-G", "--use_gpu", dest='dim', type=str,
                         help="Use GPU for training (y/n)", default='y')
-
+    parser.add_argument("-M", "--model_name", dest='model_name', type=str,
+                        help="name of model to save", defal='unnamed_model')
     args = parser.parse_args()
 
     return args
@@ -72,10 +77,10 @@ def get_vocab(trn, TEXT, pretrained_bool, dimensions):
         TEXT.build_vocab(trn)
         return TEXT
 
-def get_iterators(train_dataset, test_dataset):
+def get_iterators(train_dataset, test_dataset, batch_size):
 
         traindl, testdl = BucketIterator.splits(datasets=(train_dataset,test_dataset),
-                     batch_sizes=(50,1),
+                     batch_sizes=(batch_size,1),
                      sort_key=lambda x: len(x.lyrics),
                      device=None,
                      sort_within_batch=True,
@@ -116,6 +121,12 @@ def train(model, nr_epochs, device, training_dl):
         print("Average loss at epoch %d: %.7f" % (epoch_nr, avg_loss))
     return model
 
+def save_model(model, model_name):
+    directory = 'trained_models/'
+    if os.path.exists(directory) == False:
+        os.mkdir(directory)
+    torch.save(model.state_dict(), 'trained_models/{}.pt'.format(model_name))
+
 def evaluate(trained_model, testdl):
     correct = 0
     count = 0
@@ -140,6 +151,10 @@ def evaluate(trained_model, testdl):
 if __name__ == '__main__':
 
     args = get_args()
+    if args.gpu == 'y':
+        device = 'cuda:01'
+    else:
+        device = 'cpu'
     pretrained = args.pretrained
     dim = args.dim
     x_field, y_field = get_fields()
@@ -148,12 +163,11 @@ if __name__ == '__main__':
     print('Generating Vocab')
 
     x_field = get_vocab(train_dataset, x_field, pretrained, dim)
-    traindl, testdl = get_iterators(train_dataset, test_dataset)
+    traindl, testdl = get_iterators(train_dataset, test_dataset, args.batch_size)
     vocab_size = len(x_field.vocab)
     embedding_dim = dim
     n_out = 5
-    EPOCHS = 10
-    device = 'cuda:01'
+    EPOCHS = args.nr_epochs
     n_hidden = 128
     train_batch_it = BatchGenerator(traindl, 'lyrics', 'genre')
     test_batch_it = BatchGenerator(testdl, 'lyrics', 'genre')
@@ -161,10 +175,13 @@ if __name__ == '__main__':
 
     model = ConcatPoolingGRUAdaptive(vocab_size, embedding_dim,
                                     n_hidden, n_out, x_field.vocab.vectors,
-                                    pretrained, device).to(device)
+                                    pretrained).to(device)
 
     print(model)
     print('Training')
     trained_model = train(model, EPOCHS, device, train_batch_it)
-    print('Evaluating Model')
-    evaluate(trained_model, test_batch_it)
+
+    output_model(trained_model, args.model_name)
+
+    '''print('Evaluating Model')
+    evaluate(trained_model, test_batch_it)'''
