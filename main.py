602bd6bf42c4b805cbc261555b1e60cc4df50272
Rob <gusroberth@student.gu.se>
2020-03-30 10:31:00 +0200

    small updates

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from torchtext.data import Iterator, BucketIterator, TabularDataset, Field
 from torch import nn
-from ProjDataset import ProjDataset
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 import torch, pickle, random, os, spacy, argparse, dill
@@ -10,6 +9,10 @@ from torch.optim import Adam
 import torch.optim as optim
 from GRUNetwork import ConcatPoolingGRUAdaptive
 from BatchGenerator import BatchGenerator
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
+
 
 def get_args():
 
@@ -46,13 +49,16 @@ def get_fields():
 
 def get_dataset(TEXT, LABEL):
 
-    lyric_datafield = [("lyrics", TEXT),
+    lyric_datafield = [('id', None), ("lyrics", TEXT),
                      ("genre", LABEL)]
 
 
-    train, test = TabularDataset.splits('data/dataframes/',
-                                     train='training.csv',
-                                     test ='testing.csv',
+    train = TabularDataset('data/training.csv',
+                                     format ='csv',
+                                     fields = lyric_datafield,
+                                     skip_header=True)
+    
+    test = TabularDataset('data/testing.csv',
                                      format ='csv',
                                      fields = lyric_datafield,
                                      skip_header=True)
@@ -113,6 +119,7 @@ def train(model, nr_epochs, device, training_dl):
     epoch_nr = 0
     EPOCH = list(range(nr_epochs))
     tenp = round(len(training_dl,) / 10)
+    avg_loss_graph = []
     avg_loss = 0
     criterion = nn.CrossEntropyLoss()
     optimizer = Adam(model.parameters(), lr=0.001)
@@ -133,10 +140,13 @@ def train(model, nr_epochs, device, training_dl):
                 epoch_loss.append(loss.item())
                 optimizer.step()
                 avg_loss = sum(epoch_loss) / len(epoch_loss)
+                avg_loss_graph.append(avg_loss)
             except RuntimeError:
                 continue
+                
+             
         print("Average loss at epoch %d: %.7f" % (epoch_nr, avg_loss))
-    return model
+    return model, avg_loss_graph
 
 def save_model(model, model_name):
     directory = 'trained_models/'
@@ -147,6 +157,8 @@ def save_model(model, model_name):
 def evaluate(trained_model, testdl):
     correct = 0
     count = 0
+    actual = []
+    preds = []
     for i in tqdm(testdl):
 
         x = i[0][0]
@@ -155,16 +167,52 @@ def evaluate(trained_model, testdl):
         try:
             predictions = trained_model(x, lengths)
             for prediction in zip(predictions,y):
+                
                 count+=1
                 output, index = torch.max(prediction[0], 0)
+                actual.append(y[0].item())
+                preds.append(index.item())
                 if index.item() == y[0].item():
                     correct += 1
         except:
             continue
     accuracy = (correct / count) * 100
-
     print('Model Accuracy: {}'.format(accuracy))
+    return actual, preds
+       
+def plot_graph(data, model_name):
+    x = [i for i in range(len(data))]
+    plt.plot(x, data)
+    plt.ylabel('Average Loss/Epoch')
+    plt.xlabel('Epoch Nr')
     
+    plt.savefig('graphs/{}.png'.format(model_name), dpi=None, facecolor='w', edgecolor='w',
+        orientation='portrait', papertype=None, format='png',
+        transparent=False, bbox_inches=None, pad_inches=0.1,
+        frameon=None, metadata=None)
+
+def gen_confusion_matrix(actual, preds, model_name):
+    
+    int2genre = {0: 'Pop',
+                 1: 'Hip-Hop',
+                 2: 'Rock',
+                 3: 'Metal',
+                 4: 'Country',
+                 5: 'Jazz',
+                 6: 'Electronic'}
+    
+    y_pred = [int2genre[i] for i in preds]
+    y_actual = [int2genre[i] for i in actual]
+    con_mat = confusion_matrix(y_actual, y_pred, labels=['Pop', 'Hip-Hop', 'Rock', 'Metal', 'Country', 'Jazz', 'Electronic'])
+    
+    df_cm = pd.DataFrame(con_mat, index=['Pop', 'Hip-Hop', 'Rock', 'Metal', 'Country', 'Jazz', 'Electronic'], 
+                         columns=['Pop', 'Hip-Hop', 'Rock', 'Metal', 'Country', 'Jazz', 'Electronic'])
+    sn.set(font_scale=1.4)
+    sn.heatmap(df_cm, annot=True, annot_kws={"size": 8})
+    plt.savefig('graphs/{}con_matr.png'.format(model_name), dpi=None, facecolor='w', edgecolor='w',
+        orientation='portrait', format='png',
+        transparent=False, pad_inches=0.1)
+
 if __name__ == '__main__':
 
     args = get_args()
@@ -183,7 +231,7 @@ if __name__ == '__main__':
     traindl, testdl = get_iterators(train_dataset, test_dataset, args.batch_size)
     vocab_size = len(x_field.vocab)
     embedding_dim = dim
-    n_out = 5
+    n_out = 7
     EPOCHS = args.nr_epochs
     n_hidden = 128
     train_batch_it = BatchGenerator(traindl, 'lyrics', 'genre')
@@ -195,11 +243,13 @@ if __name__ == '__main__':
                                     n_hidden, n_out, x_field.vocab.vectors,
                                     pretrained, device).to(device)
 
-    print(model)
     print('Training')
-    trained_model = train(model, EPOCHS, device, train_batch_it)
+    trained_model, loss = train(model, EPOCHS, device, train_batch_it)
 
     save_model(trained_model, args.model_name)
-
+    plot_graph(loss,args.model_name)
     print('Evaluating Model')
-    evaluate(trained_model, test_batch_it)
+    actual, preds = evaluate(trained_model, test_batch_it)
+    conf_matrix = gen_confusion_matrix(actual, preds, args.model_name)
+    print('Done!')
+    
